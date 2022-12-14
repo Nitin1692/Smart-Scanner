232028d1b4f3dc5836cd2a8929dd812ab5bb7578
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-14 11:10:14 +0530

    Saving the file and capturing the image

--- smart_scanner.py
+++ smart_scanner.py
@@ -1,6 +1,8 @@
 import urllib.request as req
 import numpy as np
 import cv2
+from PIL import Image
+import time
 
 url = 'http://192.168.29.65:8080/shot.jpg';
 while True:
@@ -8,5 +10,13 @@ while True:
     img_bytes = bytearray(img.read())
     img_nparr = np.array(img_bytes, dtype=np.uint8)
     frame = cv2.imdecode(img_nparr, -1) # 0 for grayscale, -1 for unchanged
-    cv2.imshow('Smart Scanner', frame)
-    cv2.waitKey(1)
+    frame_cvt = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_blur = cv2.GaussianBlur(frame_cvt, (5,5), 0)
+    frame_edge = cv2.Canny(frame_blur, 30, 50)
+    cv2.imshow('Smart Scanner', frame_edge)
+    contours, h = cv2.findContours(frame_edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    if cv2.waitKey(1) == ord('s'):
+        img_pil = Image.fromarray(frame)
+        time_str = time.strftime('%Y-%m-%d-%H-%M-%S')
+        img_pil.save(f'{time_str}.pdf') 
+        print(time_str)
